c94d0f350f2623704ac4495d0b49f4d7d7ad346d
Rok Pajnič <rok.pajnic@outlook.com>
2018-05-20 00:40:33 +0200

    Tile oppening

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 
+
+$(document).ready(function () {
+    console.log("reeady", $('.overlay1')[0]);
+
+    var children = $('.overlay1').children('.overlay');
+
+    for (let i = 0; i < children.length; i++) {
+        const element = children.get(i);
+        console.log(element);
+        setTimeout(function() {
+            var ran = Math.round(Math.random() * children.length );
+            while ( $(children.get(ran)).css('opacity') != 1 ) {
+                ran = Math.round(Math.random() * children.length );
+            }
+            $(children.get(ran)).css('opacity', 0);
+        }, (i+1) * 3000); 
+    }
+});
